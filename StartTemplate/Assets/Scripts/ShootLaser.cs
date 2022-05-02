84feff0626630c974263b1e09935bafaf1b068b0
kndede <96321932+kndede@users.noreply.github.com>
2022-05-02 15:26:51 +0300

    divergent lenses added

---
+++ StartTemplate/Assets/Scripts/ShootLaser.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootLaser : MonoBehaviour
+{
+    public Material material;
+    LaserBeam beam;
+
+    private void Update()
+    {
+        if (beam != null)
+        {
+            Destroy(beam.laserObj);
+        }
+        beam = new LaserBeam(gameObject.transform.position, gameObject.transform.forward, material);
+    }
+}
